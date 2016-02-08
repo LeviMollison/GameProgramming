af97dd2024bc4057e54febdddd837f33ba59c4cc
LeviMollison <lm2817@nyu.edu>
2016-02-08 16:59:19 -0500

    what i've gotten to work so far is in myFirstTexture

--- myFirstTexture/NYUCodebase/main.cpp
+++ myFirstTexture/NYUCodebase/main.cpp
@@ -4,8 +4,8 @@
 #include <SDL.h>
 #include <SDL_opengl.h>
 #include <SDL_image.h>
-#include "ShaderProgram.cpp"
-#include "Matrix.cpp"
+#include "ShaderProgram.h"
+#include "Matrix.h"
 
 
 #ifdef _WINDOWS
@@ -16,9 +16,23 @@
 
 SDL_Window* displayWindow;
 
-float radianConverter(float deg);
-GLuint LoadTexture(const char *image_path);
-ShaderProgram program(RESOURCE_FOLDER"vertex_textured.glsl", RESOURCE_FOLDER"fragment_textured.glsl");
+GLuint LoadTexture(const char *image_path) {
+    SDL_Surface *surface = IMG_Load(image_path);
+    
+    if(surface == NULL){
+        printf("bad image\n");
+        exit(1);
+    }
+
+    GLuint textureID;
+    glGenTextures(1, &textureID);
+    glBindTexture(GL_TEXTURE_2D, textureID);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_BGRA, surface->w, surface->h, 0, GL_BGRA,GL_UNSIGNED_BYTE, surface->pixels);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    SDL_FreeSurface(surface);
+    return textureID;
+}
 
 int main(int argc, char *argv[])
 {
@@ -29,78 +43,56 @@ int main(int argc, char *argv[])
 #ifdef _WINDOWS
     glewInit();
 #endif
-    
-    glClearColor(0.5f, 0.7f, 0.1f,1.0f);
-    glClear(GL_COLOR_BUFFER_BIT);
-    
-    GLuint fireText = LoadTexture("fire2.png");
-    Matrix modelMatrix;
-    Matrix projectionMatrix;
-    Matrix viewMatrix;
-    
-    projectionMatrix.setOrthoProjection(-3.55f, 3.55f, -2.0f, 2.0f, -1.0f, 1.0f);
-    glUseProgram(program.programID);
-    
+
     SDL_Event event;
     bool done = false;
+    
+    glViewport(0, 0, 640, 360);
+    ShaderProgram program(RESOURCE_FOLDER"vertex_textured.glsl", RESOURCE_FOLDER"fragment_textured.glsl");
+    GLuint emojiTexture = LoadTexture("/Users/Akira/Documents/2016 Spring Semester NYU/Game Programing/GameProgramming/myFirstTexture/NYUCodebase/fire2.png");
+    
+    Matrix projectionMatrix;
+    Matrix modelMatrix;
+    modelMatrix.identity();
+    Matrix viewMatrix;
+    
+    projectionMatrix.setOrthoProjection(-3.55, 3.55, -2.0f, 2.0f, -1.0f, 1.0f);
+    glUseProgram(program.programID);
+    
     while (!done) {
         while (SDL_PollEvent(&event)) {
             if (event.type == SDL_QUIT || event.type == SDL_WINDOWEVENT_CLOSE) {
                 done = true;
             }
         }
-        
-        // Actual Drawing
-        program.setModelMatrix(modelMatrix);
-        program.setProjectionMatrix(projectionMatrix);
-        program.setViewMatrix(viewMatrix);
-        
-        // bind the stuff
-        glBindTexture(GL_TEXTURE_2D, fireText);
-        
-        float vertices[] = {-0.5f, -0.5f, 0.5f, -0.5f, 0.5f, 0.5f, -0.5f, -0.5f, 0.5f, 0.5f, -0.5f, 0.5f};
+        glClearColor(0.7f, 0.3f, 1.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glBindTexture(GL_TEXTURE_2D, emojiTexture);
+        float vertices[] = {-0.5, -0.5, 0.5, -0.5, 0.5, 0.5, -0.5, -0.5, 0.5, 0.5, -0.5, 0.5};
         
         glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, vertices);
         glEnableVertexAttribArray(program.positionAttribute);
         
-        float textCoords[] = {0.0f,1.0f,0.0f,0.0f,1.0f,0.0f};
-        glVertexAttribPointer(program.texCoordAttribute, 2, GL_FLOAT, false, 0, textCoords);
+        float texCoords[] = {-1.0, -1.0, 1.0, -1.0, -1.0, 1.0, -1.0, 1.0, 1.0, -1.0, 1.0, 1.0};
+        glVertexAttribPointer(program.texCoordAttribute, 2, GL_FLOAT, false, 0, texCoords);
         glEnableVertexAttribArray(program.texCoordAttribute);
+        
+        glEnable(GL_BLEND);
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+        
+        program.setModelMatrix(modelMatrix);
+        program.setProjectionMatrix(projectionMatrix);
+        program.setViewMatrix(viewMatrix);
         
         glDrawArrays(GL_TRIANGLES, 0, 6);
         
+        SDL_GL_SwapWindow(displayWindow);
+        
         glDisableVertexAttribArray(program.positionAttribute);
         glDisableVertexAttribArray(program.texCoordAttribute);
-        
-        // blend
-        glEnable(GL_BLEND);
-        
-        
-        SDL_GL_SwapWindow(displayWindow);
     }
     
     SDL_Quit();
     return 0;
-}
-
-// loading the texture
-GLuint LoadTexture(const char *image_path){
-    SDL_Surface *surface = IMG_Load(image_path);
-    GLuint textureID;
-    glGenTextures(1, &textureID);
-    
-    glBindTexture(GL_TEXTURE_2D, textureID);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_BGRA, surface->w, surface->h, 0, GL_BGRA, GL_UNSIGNED_BYTE, surface->pixels);
-    
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    SDL_FreeSurface(surface);
-    
-    return textureID;
-}
-
-// Radian Converter
-float radianConverter(float deg){
-    deg = deg * (M_PI_2/180.0);
-    return deg;
 }
